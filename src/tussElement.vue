66ce5b3bd95f66d515eabf04040ed453d0c34c5c
truonggiang2612 <toidaumuonkhocprolaanhne@gmail.com>
2022-11-01 00:10:06 +0700

    quay ve

--- src/tussElement.vue
+++ src/tussElement.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     this is vuasdassssssss
     <h1>cái này ở branch mới tạo</h1>
     <h1>cái này ở branch mới tạo</h1>
@@ -9,10 +10,15 @@
     <h1>cái này ở branch mới tạo</h1>
     <h1>cái này ở branch mới tạogit</h1>
 =======
+=======
+>>>>>>> Stashed changes
     Component này đang làm dang dở
     <ul>
       <li>qua thử git</li>
     </ul>
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
   </div>
 </template>
