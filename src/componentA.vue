160a09db992c7b52c2517645d87c9ce1de919e76
truonggiang2612 <toidaumuonkhocprolaanhne@gmail.com>
2022-10-31 23:26:29 +0700

    commit new

--- src/componentA.vue
+++ src/componentA.vue
@@ -6,11 +6,11 @@
     <h1>compoennt A</h1>
     <h1>compoennt A</h1>
     <h1>compoennt A</h1>
-    <h1>compoennt A</h1>
-    <h1>compoennt A</h1>
-    <h1>compoennt A</h1>
-    <h1>compoennt A</h1>
-    <h1>compoennt A</h1>
-    <h1>compoennt A</h1>
+    <ul>
+      <li>a</li>
+      <li>a</li>
+      <li>a</li>
+      <li>a</li>
+    </ul>
   </div>
 </template>
